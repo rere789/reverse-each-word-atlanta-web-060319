4d44cad0c755a3694d1e9d2ee2df2337cfb39b09
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 19:10:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,3 @@ def reverse_each_word(string)
   array.join(" ")
 end 
 
-# def reverse_each_word(sentence)
-#   reversed = sentence.split(" ").collect do |word|
-#   word.reverse
-# end
-#   reversed.join(" ")
-# end
